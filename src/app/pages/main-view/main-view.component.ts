f6bb53a97f567eb10ebfb216414d1009eaa396fa
Mohamed Elzohary <zohary.mo@gmail.com>
2022-12-18 06:38:03 +0200

    Initial

---
+++ src/app/pages/main-view/main-view.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/Column.model';
+
+
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.scss']
+})
+export class MainViewComponent implements OnInit {
+
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      'Awesome Idea',
+      'Good Idea'
+    ]),
+    new Column('Research', [
+      'what i have found',
+      'Great results'
+    ]),
+    new Column('To-Do', [
+      'Start working inshaAllah',
+      'Find the right team!'
+    ]),
+    new Column('Done', [
+
+    ])
+  ])
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+}
